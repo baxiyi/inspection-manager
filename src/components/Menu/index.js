5f4e0f9551d18be6d39a735f5a6799bccdc874b2
baxiyi <mzw5810@gmail.com>
2019-10-30 21:54:45 +0800

    router

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -6,18 +6,25 @@ import './index.css'
 export default class extends PureComponent {
   render() {
     return (
-      <Menu defaultSelectedKeys={['1']} mode="inline">
-          <Menu.Item key='1'>当前警告</Menu.Item>
-          <Menu.Item key='2'>历史警告</Menu.Item>
-        <Link to="/camera">
-          <Menu.Item key='3'>摄像头查看</Menu.Item>
-        </Link>
-        <Link to="/log">
-          <Menu.Item key='4'>日志系统</Menu.Item>
-        </Link>
-        <Link to="/about">
-          <Menu.Item key='5'>关于</Menu.Item>
-        </Link>
+      <Menu defaultSelectedKeys={['1']}
+        mode="inline"
+        className="menu"
+      >
+        <Menu.Item key='1'>
+          <Link to="/">当前警告</Link>
+        </Menu.Item>
+        <Menu.Item key='2'>
+          <Link to="/history">历史警告</Link>
+        </Menu.Item>
+        <Menu.Item key='3'>
+        <Link to="/camera">摄像头查看</Link>
+        </Menu.Item>
+        <Menu.Item key='4'>
+          <Link to="/log">日志系统</Link>
+        </Menu.Item>
+        <Menu.Item key='5'>
+          <Link to="/about">关于</Link>
+        </Menu.Item>
       </Menu>
     )
   }
