70294dc880d68676f6fd932a94aa099681d5e0b8
baxiyi <mzw5810@gmail.com>
2019-10-31 10:42:00 +0800

    top-bar

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -3,6 +3,15 @@ import './index.css'
 
 export default function() {
   return (
-    <div className="index">Index</div>
+    <div>
+      <div className="index">Index</div>
+      <div className="index">Index</div>
+      <div className="index">Index</div>
+      <div className="index">Index</div>
+      <div className="index">Index</div>
+      <div className="index">Index</div>
+      <div className="index">Index</div>
+      <div className="index">Index</div>
+    </div>
   )
 }
