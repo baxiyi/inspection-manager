8e659cf8f83b988ae0d6a6c45bf4950101390a9b
baxiyi <mzw5810@gmail.com>
2019-11-24 14:59:51 +0800

    time-format

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -17,6 +17,22 @@ export default class extends React.Component {
     }
   }
 
+  formatDate (date, fmt) {
+    var o = {
+        "M+": date.getMonth() + 1, //月份 
+        "d+": date.getDate(), //日 
+        "h+": date.getHours(), //小时 
+        "m+": date.getMinutes(), //分 
+        "s+": date.getSeconds(), //秒 
+        "q+": Math.floor((date.getMonth() + 3) / 3), //季度 
+        "S": date.getMilliseconds() //毫秒 
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+  }
+
   fetchCurrentWarnings() {
     fetch(`${HOST}/getCurrentWarnings.json?page=${this.state.pageOffset}&size=10`, {
       method: 'GET',
@@ -109,7 +125,7 @@ export default class extends React.Component {
           let res = {};
           res.warningId = warningId;
           res.seq = index + 1;
-          res.time = warning.occurTime;
+          res.time = this.formatDate(new Date(warning.occurTime), 'yyyy-MM-dd hh:mm:ss');
           res.isShowPic = false;
           res.imgUrls = [];
           await fetch(`${HOST}/getWarningDetail.json?warningId=${warningId}`)
